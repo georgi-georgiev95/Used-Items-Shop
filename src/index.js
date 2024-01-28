ec7b2b53d9602041e5be6f67692e01dc4a3baacd
Georgi957 <gesh007@abv.bg>
2024-01-28 13:11:49 +0200

    Add express configuration

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const { PORT } = require('./utils/constants')
 const router = require('./router');
+const expressConfig = require('./config/expressConfig');
 
 const app = express();
-
+expressConfig(app);
 app.use(router);
 
 app.listen(PORT, () => `App is listening on port: ${PORT}`);
