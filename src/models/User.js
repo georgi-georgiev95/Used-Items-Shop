e5e995badc318e32429ced2b040a03399d48228a
Georgi957 <gesh007@abv.bg>
2024-01-28 14:16:16 +0200

    Register user with hashed password

--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,26 @@
 const mongoose = require('mongoose');
 
+const bcrypt = require('bcrypt');
+
 const userSchema = new mongoose.Schema({
     username: String,
     email: String,
     password: String
 });
 
-const User = mongoose.model('User', userSchema, 'secondHandItems');
+userSchema.virtual('rePassword')
+    .set(function (value) {
+        if (value !== this.password) {
+            throw new mongoose.Error('Passwords must match!')
+        }
+    });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+})
+
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
