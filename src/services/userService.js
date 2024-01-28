cf1026ef9c17162b19dadb8007799e9b890275b5
Georgi957 <gesh007@abv.bg>
2024-01-28 14:32:02 +0200

    Login user and create cookie

--- src/services/userService.js
+++ src/services/userService.js
@@ -1,3 +1,30 @@
 const User = require('../models/User');
 
+const bcrypt = require('bcrypt');
+const jwt = require('../lib/jwt');
+const {SECRET } = require('../utils/constants');
+
 exports.register = (userData) => User.create(userData);
+
+exports.login = async (email, password) => {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Cannot find username or password!');
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Cannot find username or password');
+    }
+
+    const payload = {
+        _id: user._id,
+        email: user.email
+    }
+
+    const token = await jwt.sign(payload, SECRET, { expiresIn: '1d' });
+
+    return token;
+}
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,4 @@
 exports.PORT = 3000;
 exports.DB_URI = 'mongodb://127.0.0.1:27017/secondHandItems';
 exports.TOKEN = 'auth';
+exports.SECRET = '73ec7e7a-c080-4b41-b7b7-a40022d29715';
