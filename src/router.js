2de2781173135e52a6c4a92d784519d5852c2c2c
Georgi957 <gesh007@abv.bg>
2024-01-28 13:30:25 +0200

    Render login and register pages

--- src/router.js
+++ src/router.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
+const userController = require('./controllers/userController');
 
 router.use(homeController);
-
+router.use('/user', userController);
 router.get('*', (req, res) => {
     res.redirect('/404');
 })
