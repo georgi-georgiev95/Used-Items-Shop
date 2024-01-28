6c88cf8bcdd50a658b4051f2a83e534235238e5f
Georgi957 <gesh007@abv.bg>
2024-01-28 13:08:42 +0200

    Extract router to separate module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const express = require('express');
 
 const { PORT } = require('./utils/constants')
+const router = require('./router');
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Hello from Express!')
-});
+app.use(router);
 
 app.listen(PORT, () => `App is listening on port: ${PORT}`);
