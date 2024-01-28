c57800f4fb6c5f857629709090b00fc95e3483b7
Georgi957 <gesh007@abv.bg>
2024-01-28 13:52:05 +0200

    Add user model

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
-const { PORT } = require('./utils/constants')
+const { PORT, DB_URI } = require('./utils/constants')
 const router = require('./router');
 const expressConfig = require('./config/expressConfig');
 const handlebarsConfig = require('./config/handlebarsConfig');
 
 const app = express();
 
+mongoose.connect(DB_URI)
+    .then(() => console.log('Connected to DB'))
+    .catch(err => console.log(err));
+
 expressConfig(app);
 handlebarsConfig(app);
 
 app.use(router);
 
-app.listen(PORT, () => `App is listening on port: ${PORT}`);
+app.listen(PORT, () => console.log(`App is listening on port: ${PORT}`));
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,2 @@
 exports.PORT = 3000;
+exports.DB_URI = 'mongodb+srv://ggeorgiev9507:Ep8WrHA9DuD05dw3@cluster0.okkqwi4.mongodb.net/?retryWrites=true&w=majority'
