888eb9637cbd42900036aba5171858102e329a0e
Georgi957 <gesh007@abv.bg>
2024-01-28 13:13:30 +0200

    Configure static files in express

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -2,5 +2,6 @@ const express = require('express');
 const path = require('path');
 
 module.exports = (app) => {
-    app.use(express.urlencoded({extended: false}))
+    app.use(express.urlencoded({ extended: false }));
+    app.use(express.static(path.resolve(__dirname, '../public')));
 }
